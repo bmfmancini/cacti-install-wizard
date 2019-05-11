d16f000d1ac722accb37b7e9f2145be04e9ae95f
Sean Mancini <sean@seanmancini.com>
2019-05-11 13:14:40 -0400

    Update cacti-install-wizard-centos.sh

--- cacti-install-wizard-centos.sh
+++ cacti-install-wizard-centos.sh
@@ -19,7 +19,8 @@
 #    You should have received a copy of the GNU General Public License#
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.#
 
-
+echo "this script requires git"
+yum install -y git 
 
 
 
