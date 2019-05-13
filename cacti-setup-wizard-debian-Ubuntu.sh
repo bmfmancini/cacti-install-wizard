776883a4effcc430917789468d273289479db298
bmfmancini <sean@seanmancini.com>
2019-05-13 00:51:12 +0000

    removed php7.0-package name in favor of just php-package name tested on ubuntu 16 &  19

--- cacti-setup-wizard-debian-Ubuntu.sh
+++ cacti-setup-wizard-debian-Ubuntu.sh
@@ -56,7 +56,7 @@ fi
 
 echo "cacti requires a LAMP stack as well as some required plugins we will now install the required packages"
 apt-get update
-apt-get  install -y apache2 rrdtool mariadb-server snmp snmpd php7.0 php-mysql  libapache2-mod-php7.0   php7.0-snmp php7.0-xml php7.0-mbstring php7.0-json php7.0-gd php7.0-gmp php7.0-zip php7.0-ldap php7.0-mc
+apt-get  install -y apache2 rrdtool mariadb-server snmp snmpd php php-mysql  libapache2-mod-php   php-snmp php-xml php-mbstring php-json php-gd php-gmp php-zip php-ldap php-mc
 
 
 
