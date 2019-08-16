86325fd19c1de8155d857e494ae31e09f61848f8
bmfmancini <sean@seanmancini.com>
2019-08-16 02:09:11 +0000

    added functions and menu driven option

--- cacti-install-wizard-centos.sh
+++ cacti-install-wizard-centos.sh
@@ -19,8 +19,11 @@
 #    You should have received a copy of the GNU General Public License#
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.#
 
-echo "this script requires git"
-yum install -y git 
+
+
+
+echo "this script requires git and unzip"
+yum install -y git unzip
 
 
 
@@ -36,6 +39,8 @@ sed -i 's/enforcing/disabled/g' /etc/selinux/config /etc/selinux/config
 
 #Download chosen release
 echo "here are some of the current cacti release versions \n
+release/1.2.5
+release/1.2.4
 release/1.2.3
 release/1.2.2
 release/1.2.1
@@ -123,7 +128,7 @@ firewall-cmd --reload
 ##Timezone settings needed for cacti
 echo "Enter your PHP time zone i.e America/Toronto  Default is US/Central "
 read timezone
-if [ $timezone == "" ] 
+if [$timezone == ""] 
 then
 
 
@@ -178,7 +183,7 @@ touch $location/cacti/log/cacti.log
 chmod 664 $location/cacti/log/cacti.log
 chown -R apache:apache   $location/cacti/log/
 cp $location/cacti/include/config.php.dist $location/cacti/include/config.php
-
+chown -R apache:apache $location/cacti/include/config.php
 
 ##Create database 
 echo "would you like to customize the database name and user ? hit enter for defaults"
@@ -217,7 +222,11 @@ sed -i -e 's@^$database_password.*@$database_password = "cacti";@g' /var/www/htm
 sed -i -e 's@^$database_port.*@$database_port = "3306";@g' /var/www/html/cacti/include/config.php
 sed -i -e 's@^$database_ssl.*@$database_ssl = "false";@g' /var/www/html/cacti/include/config.php
 sed -i -e 's@^//$url_path@$url_path@g' /var/www/html/cacti/include/config.php
-
+###Cacti spine settomgs
+cp /usr/local/spine/etc/spine.conf.dist /usr/local/spine/etc/spine.conf
+sed -i -e 's@^DB_Host.*@DB_Host  127.0.0.1@g' /usr/local/spine/etc/spine.conf
+sed -i -e 's@^DB_User.*@DB_User  cacti@g' /usr/local/spine/etc/spine.conf
+sed -i -e 's@^DB_Pass.*@DB_Pass  cacti@g' /usr/local/spine/etc/spine.conf
 
 
 
@@ -267,7 +276,10 @@ sed -i -e 's@^$database_password.*@$database_password = '$customdbpassword';@g' 
 sed -i -e 's@^$database_port.*@$database_port = "3306";@g' "$location"/cacti/include/config.php
 sed -i -e 's@^$database_ssl.*@$database_ssl = "false";@g' "$location"/cacti/include/config.php
 sed -i -e 's@^//$url_path@$url_path@g' $location/cacti/include/config.php
-
+cp /usr/local/spine/etc/spine.conf.dist /usr/local/spine/etc/spine.conf
+sed -i -e 's@^DB_Host.*@DB_Host  127.0.0.1@g' /usr/local/spine/etc/spine.conf
+sed -i -e 's@^DB_User.*@DB_User  '$customdbuser'@g' /usr/local/spine/etc/spine.conf
+sed -i -e 's@^DB_Pass.*@DB_Pass  '$customdbpassword'@g' /usr/local/spine/etc/spine.conf
 
 fi
 
@@ -279,7 +291,8 @@ fi
 sed -e 's/max_execution_time = 30/max_execution_time = 60/' -i /etc/php.ini
 sed -e 's/memory_limit = 128M/memory_limit = 400M/' -i /etc/php.ini
 
-
+###Make a backup of maria db config before making changes 
+cp /etc/my.cnf.d/server.cnf /etc/my.cnf.d/server.cnf.backup
 
 echo "Applying recommended DB settings"
 echo "
@@ -292,6 +305,7 @@ innodb_large_prefix = 1
 innodb_buffer_pool_size = 250M
 innodb_additional_mem_pool_size = 90M
 innodb_flush_log_at_trx_commit = 2
+innodb_doublewrite = OFF
 " >> /etc/my.cnf.d/server.cnf
 
 
@@ -300,7 +314,7 @@ innodb_flush_log_at_trx_commit = 2
 
 echo "this script can download the following plugins monitor,thold would you like to install them  ? type yes to download hit enter to skip"
 read plugins
- if [ $plugins == "yes" ]
+ if [ $plugins = "yes" ]
   then
    git clone https://github.com/Cacti/plugin_thold.git
     git clone https://github.com/Cacti/plugin_monitor.git
@@ -319,6 +333,8 @@ echo "*/5 * * * * $user php $location/cacti/poller.php > /dev/null 2>&1" > /etc/
 
 
 
+
+
 echo "refreshing services"
 systemctl restart httpd
 systemctl restart mariadb
@@ -326,4 +342,175 @@ systemctl restart mariadb
 
 echo "Installation completed !"
 
+
+
+function spine_install () {
+
+
+##Download packages needed for spine
+
+yum install -y gcc mysql-devel net-snmp-devel autoconf automake libtool dos2unix help2man
+
+echo " Which version of spine would you like to use ? Hit enter for the latest or enter the release vesrion i.e 1.2.3 Usually should Match your installed version of Cacti"
+read version
+
+if [$version = ""]
+then
+
+echo "downloadinglatest version of spine  and compling "
+git clone https://github.com/Cacti/spine.git
+cd spine
+./bootstrap
+./configure
+make
+make install
+chown root:root /usr/local/spine/bin/spine
+chmod u+s /usr/local/spine/bin/spine
+
+else
+
+wget https://github.com/Cacti/spine/archive/release/$version.zip
+unzip $version.zip
+cd spine-release-$version
+./bootstrap
+./configure
+make
+make install
+chown root:root /usr/local/spine/bin/spine
+chmod u+s /usr/local/spine/bin/spine
+
+fi
+
+cp /usr/local/spine/etc/spine.conf.dist  /usr/local/spine/etc/spine.conf
+
+
+
+echo "Spine has been compiled and installed you will now need to configure your DB credentials to /usr/local/spine/etc/spine.conf"
+
+echo "Would you like to configure you spine.conf file? y or n"
+read answer
+if [ $answer == "y" ]
+then
+echo "Enter database user"
+read user
+echo "enter database password"
+read password
+echo "enter database name"
+read databasename
+
+sed -i -e 's@^DB_Host.*@DB_Host  127.0.0.1@g' /usr/local/spine/etc/spine.conf
+sed -i -e 's@^DB_User.*@DB_User  '$user'@g' /usr/local/spine/etc/spine.conf
+sed -i -e 's@^DB_Pass.*@DB_Pass  '$password'@g' /usr/local/spine/etc/spine.conf
+sed -i -e 's@^DB_Database.*@DB_Database  '$databasename'@g' /usr/local/spine/etc/spine.conf
+
+else
+
+
+echo "spine install completed"
+
+fi
+
+}
+
+
+function cacti_upgrade () {
+
+echo "Stopping cron service"
+systemctl stop cron
+
+
+echo "this option will upgrade you existing cacti installation
+you will need to supply you db information and cacti installation path"
+
+echo "specify your db username"
+read currentdbuser
+
+echo "specify your database name"
+read currentdb
+echo "specify your current db password"
+read currentdbpwd
+echo "specify your cacti install path usually /var/www/html"
+read currentpath
+
+
+echo "backing up DB"
+mysql -u $currentdbuser -p $currentdbpassword + " " $currentdb > cacti_db_backup.sql
+
+#echo "backup current install files"
+#cp -R $currentpath .
+
+
+echo  "which release would you like to upgrade to? Hit enter for latest"
+read version
+
+if  [ "$version" == "" ]
+then
+git clone https://github.com/Cacti/cacti.git
+
+
+else 
+wget https://github.com/Cacti/cacti/archive/release/$version.zip
+unzip $version 
+mv cacti-release-$version cacti
+fi
+
+
+mv $currentpath/cacti  /tmp
+mv cacti $currentpath
+
+echo "adding old config.php file into new cacti folder"
+cp /tmp/cacti/include/config.php $currentpath/cacti/include/config.php
+
+echo "Moving plugin files back into new cacti folder"
+cp -R /tmp/cacti/plugins/* $currentpath/cacti/plugins/
+
+chown -R www-data:www-data $currentpath
+
+
+echo "what system user do you run cacti as ? usually www-data"
+read cactiuser
+
+chown -R $cactiuser:$cactiuser $currentpath/cacti
+
+
+
+echo "cacti has been upgraded to  " + $version
+echo " a backup of your previous release has been made to /tmp"
+echo "once you have confirmed everything is working remove the backup from /tmp"
+
+
+
+
+systemctl start cron
+
+}
+
+
+
+
+#####Menu 
+
+
+
+
+choice='Select a operation: '
+options=("New-installation" "spine-only-installation" "cacti-upgrade" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "New-installation")
+            new_install
+            ;;
+        "spine-only-installation")
+            spine_install
+            ;;
+        "cacti-upgrade")
+            cacti_upgrade
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo invalid option;;
+    esac
+done
 
