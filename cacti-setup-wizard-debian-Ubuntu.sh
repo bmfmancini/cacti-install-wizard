0c8ca14b7d8246cf368d4d3221b296e9df3fbf60
bmfmancini <sean@seanmancini.com>
2019-09-05 23:01:41 +0000

    fixed download options for plugins

--- cacti-setup-wizard-debian-Ubuntu.sh
+++ cacti-setup-wizard-debian-Ubuntu.sh
@@ -35,13 +35,15 @@ function new_install () {
 #Download chosen release
 
 echo "here are some of the current cacti release versions \n
+release/1.2.6
+release/1.2.5
 release/1.2.4
 release/1.2.3
 release/1.2.2
 release/1.2.1
 release/1.2.0
 "
-
+###One day I will have this auto populate 
 
 
 echo  "which release would you like to download ? Hit enter for latest"
@@ -278,7 +280,7 @@ sed -e 's/memory_limit = 128M/memory_limit = 400M/' -i /etc/php/$php_version/apa
 
 echo "this script can download the following plugins monitor,thold,audit from the cacti group  would you like to install them  ? type yes to download hit enter to skip"
 read plugins
- if [ $plugins = "yes" ]
+ if [[  $plugins == "yes"  ]]
   then
    git clone https://github.com/Cacti/plugin_thold.git  thold
     git clone https://github.com/Cacti/plugin_monitor.git monitor
@@ -303,7 +305,7 @@ else
 
 echo "Would you like to download my RRD Monitoring script ? type yes to download hit enter to skip "
 read mon_script
-if [ $mon_script = "yes" ]
+if [[ $mon_script == "yes" ]]
   then
   git clone  https://github.com/bmfmancini/rrd-monitor.git
       else
