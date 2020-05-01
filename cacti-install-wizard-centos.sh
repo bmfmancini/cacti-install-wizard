cd804e49dc855c3bced99b3164ec466882eedc20
root <root@li933-191.members.linode.com>
2020-05-01 16:40:09 +0000

    fix php timezone prompt

--- cacti-install-wizard-centos.sh
+++ cacti-install-wizard-centos.sh
@@ -137,7 +137,7 @@ firewall-cmd --reload
 ##Timezone settings needed for cacti
 echo "Enter your PHP time zone i.e America/Toronto  Default is US/Central "
 read timezone
-if [ $timezone == "" ] 
+if [$timezone = ""] 
 then
 
 
